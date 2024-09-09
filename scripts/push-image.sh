bacad6f2fde2d98c773ea4bf4647f619406a1b87
Sumedh Vartak <67580847+sumedh004@users.noreply.github.com>
2024-09-09 16:55:33 +1000

    update image deploy script

--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -11,18 +11,5 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
-if [ -z "$REGISTRY_UN" ]; then
-  echo "Error: REGISTRY_UN is not set."
-  exit 1
-fi
-
-
-if [ -z "$REGISTRY_PW" ]; then
-  echo "Error: REGISTRY_UN is not set."
-  exit 1
-fi
-
-
-echo $REGISTRY_PW | docker login $CONTAINER_REGISTRY --username $REGISTRY_UN --password-stdin
 docker push $CONTAINER_REGISTRY/book_catalog:$VERSION
 docker push $CONTAINER_REGISTRY/inventory_management:$VERSION
